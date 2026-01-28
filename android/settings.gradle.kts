f92361880f82e2731545b55c7c19b3dcf603ae2a
Li <1213883194@qq.com>
2026-01-28 17:46:25 +0800

    build(android): 添加阿里云镜像仓库以加速依赖下载
    添加阿里云 Maven 镜像仓库和 Flutter 国内镜像，用于替换默认的 Google 和 Maven Central 仓库，以解决国内开发者构建时因网络问题导致的依赖下载缓慢或失败的问题。

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,12 +11,22 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://storage.flutter-io.cn/download.flutter.io") }
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
+dependencyResolutionManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://storage.flutter-io.cn/download.flutter.io") }
+    }
+}
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
